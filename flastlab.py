99a6407d38d2c1afcaa4e7ea14e9ce36fc76823c
Coolpepper74 <Coolpepper74@mail.ru>
2023-04-28 10:37:29 +0700

    Add extra URL

--- flastlab.py
+++ flastlab.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import pydantic
 
 app = FastAPI()
@@ -10,6 +13,13 @@ def sum_two_args(x,y):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+@app.get("/some_url/{something}", response_class=HTMLResponse)
+async def read_something(request: Request, something: str):
+ return templates.TemplateResponse("some.html", {"request": request,
+"something": something})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
